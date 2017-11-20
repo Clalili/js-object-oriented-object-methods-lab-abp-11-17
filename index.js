186de335f3203f9aaa3293ffebac78bf2ba83fbd
Clara <c_c_456@hotmail.com>
2017-11-20 19:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function BoardMember(name, homeState, training){
   this.training = training
 
   this.veto = function(){
-    return 'No, I must disagree'
+    return "No, I must disagree"
   }
 
   this.approve = function(){
@@ -18,7 +18,7 @@ function BoardMember(name, homeState, training){
   this.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
-  
+
   this.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`
   }
